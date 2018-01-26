b2298416318bfc104666ad8db4b85360c15cd1de
Federico Palomero Garcia <fedepalo13@gmail.com>
2018-01-26 17:49:39 -0300

    executor

---
+++ src/Executor/MyTask.java
@@ -0,0 +1,23 @@
+package Executor;
+
+import java.time.LocalTime;
+
+public class MyTask implements Runnable{
+
+    private String name;
+
+    public MyTask(String name){
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println(LocalTime.now() +": "+
+        Thread.currentThread().getName()+ "is exeuting"+ name);
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
