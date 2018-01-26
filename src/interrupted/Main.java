e9f59a2ec974a3b99c45f1f120a598a8c6a46840
Federico Palomero Garcia <fedepalo13@gmail.com>
2018-01-26 16:44:53 -0300

    interrupted threads

---
+++ src/interrupted/Main.java
@@ -0,0 +1,15 @@
+package interrupted;
+
+public class Main {
+    public static void main(String[] args) {
+        ThreadGroup group=new ThreadGroup("Group");
+
+        for (int i=0;i<5; i++){
+            Thread thread=new Thread(group,new Task(), "Thread"+i);
+            thread.start();
+        }
+        group.interrupt();
+
+        System.out.println("Finished"+Thread.currentThread().getName());
+    }
+}
---
+++ src/interrupted/Task.java
@@ -0,0 +1,13 @@
+package interrupted;
+
+public class Task implements Runnable {
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(10000000);
+        }catch (InterruptedException e){
+            System.out.println("Interrupted"+Thread.currentThread().getName());
+        }
+        System.out.println("Finished"+Thread.currentThread().getName());
+    }
+}
